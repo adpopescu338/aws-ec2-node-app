7d072662c6bf30674aa090b00951e822d3a4b5cc
Alex <ad.popescu338@yahoo.com>
2023-02-22 00:45:16 +0000

    devopsy

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
-
+// serve static files from the public folder
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
-

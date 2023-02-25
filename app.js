4dfd15f48d8460eadf8f5be3a006b4206ea1d1c6
Alex <ad.popescu338@yahoo.com>
2023-02-25 00:24:08 +0000

    made dev branch

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/hello/:name", (req, res) => {
+  console.log("Api works. Hello World!");
+  res.send(`Hello ${req.params.name}!`);
+});
+
 // serve static files from the public folder
 app.use(express.static("public"));
 
